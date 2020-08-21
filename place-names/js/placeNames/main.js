f907987d4414d9a421601df99d1d3e22bac59884
Zach McWhirter <zmac7811@gmail.com>
2020-08-21 16:06:47 -0500

    translated placeNames to js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,28 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
 
+  console.log("All place names");
+
+  for (let name of names) {
+    console.log(name)
+  };
+  console.log('\n');
+  console.log("'The' Place Names");
+
+
+  names.filter(name => {
+    if (name.startsWith("The")) {
+      console.log(name);
+    }
+  });
+
+
+
+  for (let name of theNames) {
+    console.log(name);
+  }
 }
 
 main();
