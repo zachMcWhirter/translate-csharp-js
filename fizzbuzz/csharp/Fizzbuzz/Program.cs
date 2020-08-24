dd8355ddec96bd0219b4a874f963d117f8b2a5a8
Zach McWhirter <zmac7811@gmail.com>
2020-08-24 18:11:24 -0500

    Fizzbuzz complete

---
+++ fizzbuzz/csharp/Fizzbuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
