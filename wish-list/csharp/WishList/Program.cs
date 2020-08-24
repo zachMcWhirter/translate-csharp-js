33d441fca04e70ced443ec4dbd64409daac546cb
Zach McWhirter <zmac7811@gmail.com>
2020-08-24 14:50:40 -0500

    Wish List complete

---
+++ wish-list/csharp/WishList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wishes = new List<string>
+            {
+                "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'",
+                "The ability to think of interesting wishes on short notice",
+                "A washtub of Skittles",
+                "World peace, but not the kind of world peace where all the humans are removed from existence",
+                "A TV that is NOT internet-capable",
+                "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"
+            };
+
+            Console.WriteLine("My wishes...");
+
+            foreach (string wish in wishes)
+            {
+                Console.WriteLine(wish);
+            }
+
+        }
+    }
+}
