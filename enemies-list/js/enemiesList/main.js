a4aea98402e3589e2c73c130098e5bf2a9fd876e
Zach McWhirter <zmac7811@gmail.com>
2021-04-15 13:48:56 -0500

    enemies-list translated

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,6 +1,54 @@
-function main() {
+
+const enemies = [
+  {
+    firstName: "Joshua",
+    lastName: "Flowers",
+    offenses: [
+      "Being a jerk to me in elementary school",
+      "Not being nice to me in elementary school"
+    ],
+    isReallyHated: true
+  },
+  {
+    firstName: "Darth",
+    lastName: "Vader",
+    offenses: [
+      "Cut off Luke's hand",
+      "Murdered all those kids",
+      "Unkind management practices"
+    ],
+    isReallyHated: false
+  },
+  {
+    firstName: "Betty",
+    lastName: "Rudelady",
+    offenses: [
+      "Phone calls in the theater",
+      "Phone calls on the bus",
+      "Phone calls in line at the grocery store",
+      "Poor conversationalist"
+    ],
+    isReallyHated: true
+  },
+  {
+    firstName: "Leon",
+    lastName: "Peck",
+    offenses: [
+      "Keeps giving me a hotplate"
+    ],
+    isReallyHated: false
+  }
+]
+
+const main = () => {
   // Put your code here
+  console.log("My Enemies List!");
+  console.log("----------------");
 
-}
+  for (let myEnemy of enemies)
 
+    myEnemy.isReallyHated
+      ? console.log(`${myEnemy.firstName} ${myEnemy.lastName} (Really, really dislike!)`)
+      : console.log(`${myEnemy.firstName} ${myEnemy.lastName}`)
+};
 main();
